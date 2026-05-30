99b8b4621f023df5d7794e09040688daf80bf2a9
Arjun Soni <arjun.official338@gmail.com>
2026-05-31 00:47:06 +0530

    fix: board sync bug and O(1) bitmask validity for solver
    - Remove diverging board member from sudoku_cracker; solveSudoku now
      operates on the passed-in board reference exclusively, so the solved
      state is always consistent with what main() prints.
    - Replace isvalid() row/col/box O(9) scan with three uint16_t bitmask
      arrays (rows[], cols[], boxes[]). Validity is now a single bitwise AND,
      reducing work per candidate check from 27 comparisons to 3.
    - Initialize bitmasks from the board before solving; update on place/unplace.

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,159 +1,127 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include <cstdint>
 #include <cstdlib>
 #include <ctime>
-
-/* we are making a sudoku.
-*/
+#include <iostream>
+#include <vector>
 
 using namespace std;
-class sudoku_cracker
-{
-private:
-    vector<vector<char>> board;
 
-    bool isvalid(vector<vector<char>> &board, int row, int col, char c)
-    {
-        for (int k = 0; k < 9; k++)
-        {
-            if (board[row][k] == c)
-                return false; // this checks the whole row!
-            if (board[k][col] == c)
-                return false; // this checks the while col!
-            // this is important, checking the sub matrix! it has a formula
-            if (board[3 * (row / 3) + (k / 3)][3 * (col / 3) + (k % 3)] == c)
-                return false;
-        }
-        return true;
-    }
-    bool solve(vector<vector<char>> &board)
-    {
-        for (int i = 0; i < board.size(); i++)
-        {
-            for (int j = 0; j < board[i].size(); j++)
-            {
-                if (board[i][j] == '.')
-                {
-                    for (char c = '1'; c <= '9'; c++)
-                    {
-                        if (isvalid(board, i, j, c))
-                        {
-                            board[i][j] = c; // here we inserted the character at the place of '.'
-                            if (solve(board) == true)
-                            {
-                                return true;
-                            }
-                            else
-                            {
-                                board[i][j] = '.';
-                            }
-                        }
-                    }
-                    return false;
-                }
+class sudoku_cracker {
+private:
+  uint16_t rows[9], cols[9], boxes[9];
+
+  bool isvalid(int r, int c, int d) {
+    uint16_t mask = 1 << (d - 1);
+    return !(rows[r] & mask) && !(cols[c] & mask) &&
+           !(boxes[(r / 3) * 3 + c / 3] & mask);
+  }
+
+  bool solve(vector<vector<char>> &board) {
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        if (board[i][j] == '.') {
+          int box = (i / 3) * 3 + j / 3;
+          for (int d = 1; d <= 9; d++) {
+            if (isvalid(i, j, d)) {
+              uint16_t mask = 1 << (d - 1);
+              board[i][j] = '0' + d;
+              rows[i] |= mask;
+              cols[j] |= mask;
+              boxes[box] |= mask;
+
+              if (solve(board))
+                return true;
+
+              board[i][j] = '.';
+              rows[i] &= ~mask;
+              cols[j] &= ~mask;
+              boxes[box] &= ~mask;
             }
+          }
+          return false;
         }
-        return true;
+      }
     }
+    return true;
+  }
 
 public:
-    sudoku_cracker(const vector<vector<char>> &initialBoard)
-    {
-        board = initialBoard;
-    }
+  bool solveSudoku(vector<vector<char>> &board) {
+    for (int i = 0; i < 9; i++)
+      rows[i] = cols[i] = boxes[i] = 0;
 
-    bool solveSudoku(vector<vector<char>> &board)
-    {
-        return solve(board);
-    }
-
-    void printBoard()
-    {
-        for (auto &row : board)
-        {
-            for (auto &cell : row)
-                cout << cell << " ";
-            cout << endl;
+    for (int i = 0; i < 9; i++)
+      for (int j = 0; j < 9; j++)
+        if (board[i][j] != '.') {
+          int d = board[i][j] - '0';
+          uint16_t mask = 1 << (d - 1);
+          rows[i] |= mask;
+          cols[j] |= mask;
+          boxes[(i / 3) * 3 + j / 3] |= mask;
         }
-    }
+
+    return solve(board);
+  }
 };
 
-void genRandomSudoku(vector<vector<char>> &board)
-{
-    // Step 1: Create an empty board
-    board = vector<vector<char>>(9, vector<char>(9, '.'));
-
-    // Step 2: Fill diagonal 3x3 boxes to reduce complexity
-    //here we are using lamda function-> [capture_list](parameter_list) -> return_type{...... }; -> l
-    auto fillBox = [&](int row, int col)
-    {
-        vector<char> nums = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-        random_shuffle(nums.begin(), nums.end());
-        int idx = 0;
-        for (int i = row; i < row + 3; ++i)
-        {
-            for (int j = col; j < col + 3; ++j)
-            {
-                board[i][j] = nums[idx++];
-            }
-        }
-    };
-
-    fillBox(0, 0);//0'st subgrid(3X3)
-    fillBox(3, 3);//4'th subgrid(3X3)
-    fillBox(6, 6);//8'th subgrid(3X3)
-
-    // Step 3: Solve the board completely using sudoku_cracker logic
-    sudoku_cracker filler(board);//here this filler is and object of the sudoku_cracker class
-    filler.solveSudoku(board); // Now board is fully solved
-
-    // Step 4: Remove random 40-50 cells to create the puzzle
-    int cells_to_remove = 50;//as removing 50+ element form the total of 81 elements from solved matrix of (9X9) makes it harder! 
-    while (cells_to_remove > 0)
-    {
-        int i = rand() % 9;
-        int j = rand() % 9;
-        if (board[i][j] != '.')
-        {
-            board[i][j] = '.';
-            cells_to_remove--;
-        }
-    }
+void printBoard(const vector<vector<char>> &board) {
+  for (auto &row : board) {
+    for (auto &cell : row)
+      cout << cell << " ";
+    cout << "\n";
+  }
 }
 
-int main()
-{
-    srand(time(0)); // Seed the random number generator FIRST, if we want that rand() generates random values then we need to seed it first.
+void genRandomSudoku(vector<vector<char>> &board) {
+  board = vector<vector<char>>(9, vector<char>(9, '.'));
 
-    vector<vector<char>> board;
-    genRandomSudoku(board); // board is now filled with a Sudoku puzzle
+  auto fillBox = [&](int row, int col) {
+    vector<char> nums = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    random_shuffle(nums.begin(), nums.end());
+    int idx = 0;
+    for (int i = row; i < row + 3; ++i)
+      for (int j = col; j < col + 3; ++j)
+        board[i][j] = nums[idx++];
+  };
 
-    sudoku_cracker prime_solver(board); // object created with the puzzle, it is the object of class sudoku_cracker, and with object we called the parameterized constructor in the class!  
+  fillBox(0, 0);
+  fillBox(3, 3);
+  fillBox(6, 6);
 
-    cout << "Sudoku Puzzle:\n";
-    prime_solver.printBoard();
+  sudoku_cracker filler;
+  filler.solveSudoku(board);
 
-    cout << "\nSolving...\n\n";
-    cout << "\nthe sudocraft\n\n";
-
-    if (prime_solver.solveSudoku(board)) // Solve the puzzle
-    {
-        // print the solved sudoku!
-        cout << "Solved Sudoku:\n";
-        for (int i = 0; i < board.size(); i++)
-        {
-            for (int j = 0; j < board[i].size(); j++)
-            {
-                cout << board[i][j] << " ";
-            }
-            cout << endl;
-        }
+  int cells_to_remove = 50;
+  while (cells_to_remove > 0) {
+    int i = rand() % 9;
+    int j = rand() % 9;
+    if (board[i][j] != '.') {
+      board[i][j] = '.';
+      cells_to_remove--;
     }
-    else
-    {
-        cout << "Sudoku is unsolvable.\n";
-    }
+  }
+}
 
-    return 0;
-} 
+int main() {
+  srand(time(0));
+
+  vector<vector<char>> board;
+  genRandomSudoku(board);
+
+  cout << "Sudoku Puzzle:\n";
+  printBoard(board);
+
+  cout << "\nSolving...\n\n";
+  cout << "the sudocraft\n\n";
+
+  sudoku_cracker solver;
+  if (solver.solveSudoku(board)) {
+    cout << "Solved Sudoku:\n";
+    printBoard(board);
+  } else {
+    cout << "Sudoku is unsolvable.\n";
+  }
+
+  return 0;
+}
